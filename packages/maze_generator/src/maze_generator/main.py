bb63461c4cd7021bf57d00b3009a5e00746084a6
Vincent Poulailleau <vpoulailleau@gmail.com>
2026-03-25 16:09:49 +0100

    display shortest path

--- packages/maze_generator/src/maze_generator/main.py
+++ packages/maze_generator/src/maze_generator/main.py
@@ -1,17 +1,23 @@
 """Main entry point for maze-generator."""
 
+from typing import TYPE_CHECKING
+
 from maze_generator import generate_maze
+
+if TYPE_CHECKING:
+    from maze_generator.maze import Path
 
 
 def main() -> None:
     """Run the maze generator application."""
     maze = generate_maze(30, 30)
-    paths = maze.paths(0, 0, 29, 29)
+    paths: list[Path] = maze.paths(0, 0, 29, 29)
     print(maze)  # noqa: T201
     print(f"Number of paths from (0, 0) to (29, 29): {len(paths)}")  # noqa: T201
     if paths:
-        print("Example path:", paths[0])  # noqa: T201
-        print(maze.highlighted_path(paths[0]))  # noqa: T201
+        shortest_path = min(paths, key=len)
+        print("Example path:", shortest_path)  # noqa: T201
+        print(maze.highlighted_path(shortest_path))  # noqa: T201
 
 
 if __name__ == "__main__":
